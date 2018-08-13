de3cd2a5834fb00a5d173a5aeb46ff0dc64f43d4
daxdax89 <daxdax89@msn.com>
2018-08-13 20:09:37 +0200

    Images fixed

--- src/components/BlurbSection.vue
+++ src/components/BlurbSection.vue
@@ -3,22 +3,22 @@
         <div id="content">
             <div class="columns">
                 <div class="column blurb">
-                    <div class="image is-128x128 blurb-image">
-                        <img src="../assets/blurb/1.png">
+                    <div class="blurb-image">
+                        <img src="../assets/blurb/1.png" class="image is-128x128">
                     </div>
                     <h2 class="blurb-title">A new era in supply chain ecosystems</h2>
                     <p class="blurb-text">From Agri-Food to Technology value chains, HERC can be used for any physical interchangeable item and any entity type of organization in virtually any local, regional, or Global Value Chain.</p>
                 </div>
                 <div class="column blurb">
-                    <div class="image is-128x128 blurb-image">
-                        <img src="../assets/blurb/2.png">
+                    <div class="blurb-image">
+                        <img src="../assets/blurb/2.png" class="image is-128x128">
                     </div>
                     <h2 class="blurb-title">100% Provable, <br>24-hours a day</h2>
                     <p class="blurb-text">Enabling transparent and efficient supply chain management functions.</p>
                 </div>
                 <div class="column blurb">
-                    <div class="image is-128x128 blurb-image">
-                        <img src="../assets/blurb/3.png">
+                    <div class="blurb-image">
+                        <img src="../assets/blurb/3.png" class="image is-128x128">
                     </div>
                     <h2 class="blurb-title">Measure, Verify , Track</h2>
                     <p class="blurb-text">Blockchain software for tracking physical inventory in a Supply Chain with a digital source to store and review verification information.</p>
@@ -26,22 +26,22 @@
             </div>
             <div class="columns">
                 <div class="column blurb">
-                    <div class="image is-128x128 blurb-image">
-                        <img src="../assets/blurb/4.png">
+                    <div class="blurb-image">
+                        <img src="../assets/blurb/4.png" class="image is-128x128">
                     </div>
                     <h2 class="blurb-title">Dedicated to decentralized distribution</h2>
                     <p class="blurb-text">A robust ecosystem for entities dedicated to decentralized distribution and attaining effective supply chain planning production for commodities.</p>
                 </div>
                 <div class="column blurb">
-                    <div class="image is-128x128 blurb-image">
-                        <img src="../assets/blurb/5.png">
+                    <div class="blurb-image">
+                        <img src="../assets/blurb/5.png" class="image is-128x128">
                     </div>
                     <h2 class="blurb-title">Revolutionizing the global value chain</h2>
                     <p class="blurb-text">HERC helps to create a more secure and streamlined ecosystem and is the easiest way to verify supply chain items and proving their existence, possession, storage and insurance in crypto units.</p>
                 </div>
                 <div class="column blurb">
-                    <div class="image is-128x128 blurb-image">
-                        <img src="../assets/blurb/6.png">
+                    <div class="blurb-image">
+                        <img src="../assets/blurb/6.png" class="image is-128x128">
                     </div>
                     <h2 class="blurb-title">Helping Eradicate Racketeering & Collusion</h2>
                     <p class="blurb-text">Empowering & entrusting the public with control of the Global Value Chain.</p>
@@ -74,11 +74,9 @@ export default {
   margin: 5%;
   margin-bottom: auto;
   padding: 20px;
-  
 }
 
-.blurb-image {
-  width: 100%;
+img {
   margin: auto;
   margin-bottom: 10%;
 }
@@ -120,8 +118,8 @@ export default {
     font-size: 10px;
   }
 
-  .blurb{
-      margin: 2%;
+  .blurb {
+    margin: 2%;
   }
 }
 
@@ -131,9 +129,9 @@ export default {
     font-size: 10px;
   }
 
-  .columns{
+  .columns {
     margin-bottom: 10% !important;
-}
+  }
 }
 
 /*** Responsive Styles Smartphone Portrait ***/
