6f7d4f64ab476cd3872011717356b9e33956bc7f
daxdax89 <daxdax89@msn.com>
2018-08-14 18:22:51 +0200

    AWS Setup

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pluginOptions: {
+    s3Deploy: {
+      assetPath: 'dist',
+      bucket: 'www.herc.one',
+      region: 'us-east-2',
+      pwa: false,
+      enableCloudfront: true,
+      cloudfrontId: 'EXLWYOT38MF3W',
+      cloudfrontMatchers: '/*',
+      uploadConcurrency: 5
+    }
+  }
+}
