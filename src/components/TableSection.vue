8dbd2bbd29eafe5a020c17e1bc79f29026bfd80e
DaX <daxdax89@msn.com>
2018-08-15 23:36:37 +0200

    Table Backend Done

--- src/components/TableSection.vue
+++ src/components/TableSection.vue
@@ -47,12 +47,12 @@
                     <tr>
                         <td class="factom-color"><span id="hercNeeded"></span><br>HERC needed</td>
                         <td class="storj-color"><span id="hercNeeded2"></span><br>HERC needed</td>
-                        <td class="ipfs-color"><span id="hercNeeded3"></span><br>HERC needed</td>
+                        <td class="ipfs-color"><span id="hercNeeded3">0</span><br>HERC needed</td>
                     </tr>
                     <tr>
                         <td class="factom-color"><span id="ethGasPrice"></span><br>ETH Gas Price</td>
                         <td class="storj-color"><span id="ethGasPrice2"></span><br>ETH Gas Price</td>
-                        <td class="ipfs-color"><span id="ethGasPrice"></span><br>ETH Gas Price</td>
+                        <td class="ipfs-color"><span id="ethGasPrice3"></span><br>ETH Gas Price</td>
                     </tr>
                 </tbody>
             </table>
@@ -61,74 +61,69 @@
 </template>
 
 <script>
+import JQuery from "jquery";
+let $ = JQuery;
 export default {
   name: "TableSection"
-  // mounted: function() {
-  //   this.$nextTick(function() {
-  //     // Code that will run only after the
-  //     // entire view has been rendered
-
-  //     alert("lol!");
-  //   });
-  // },
-  // roundUp: function(num, precision) {
-  //   precision = Math.pow(10, precision);
-  //   return Math.ceil(num * precision) / precision;
-  //   alert("Round up done!");
-  //   console.log("test");
-  // }
 };
 
 // jQuery(function($) {
-//   function roundUp(num, precision) {
-//     precision = Math.pow(10, precision)
-//     return Math.ceil(num * precision) / precision
-//   }
+function roundUp(num, precision) {
+  precision = Math.pow(10, precision);
+  return Math.ceil(num * precision) / precision;
+}
 
-//   roundUp(192.168, 1) //=> 192.2
-//   $.getJSON('https://jsondata.herc.one/service-1.0-SNAPSHOT/JSON', function(data) {
+roundUp(192.168, 1); //=> 192.2
+$.getJSON("https://jsondata.herc.one/service-1.0-SNAPSHOT/JSON", function(
+  data
+) {
+  var roundedUp;
+  var factPrice = `${data.factomPrice}`;
+  var storjPrice = `${data.storjPrice}`;
+  var etheriumNeeded = `${data.gasPrice}`;
+  var hercPrice = 0.6;
 
-//     var roundedUp;
-//     var factPrice = `${data.factomPrice}`;
-//     var storjPrice = `${data.storjPrice}`;
-//     var etheriumNeeded = `${data.gasPrice}`;
-//     var hercPrice = 0.60;
+  var factNeeded = 0.000128 / factPrice;
+  var storjNeeded = 0.0007032 / storjPrice;
+  var hercBurned = factNeeded / hercPrice;
 
-//     var factNeeded = 0.000128 / factPrice;
-//     var storjNeeded = 0.0007032 / storjPrice;
-//     var hercBurned = factNeeded/hercPrice;
+  factNeeded = roundUp(factNeeded, 6);
+  storjNeeded = roundUp(storjNeeded, 6);
 
-//     factNeeded = roundUp(factNeeded, 6);
-//     storjNeeded = roundUp(storjNeeded, 6);
+  var hercNeededFCT = 0.000128 / 0.6 * factPrice;
+  var hercNeededSTORJ = 0.0007032 / 0.6 * storjPrice;
+  var hercNeeded =
+    storjNeeded / hercPrice +
+    factNeeded / hercPrice +
+    etheriumNeeded / hercPrice +
+    hercBurned;
 
-//     var hercNeededFCT = (0.000128 / 0.60) * factPrice;
-//     var hercNeededSTORJ = (0.0007032 / 0.60) * storjPrice;
-//     var hercNeeded = ((storjNeeded/hercPrice) + (factNeeded/hercPrice)+(etheriumNeeded/hercPrice)+hercBurned);
+    hercNeededFCT = roundUp(hercNeededFCT, 6);
+    hercNeededSTORJ = roundUp(hercNeededSTORJ, 6);
 
-//     $("#factPrice").html(factPrice);
-//     $("#factPriceMobile").html(factPrice);
-//     $("#storjPrice").html(storjPrice);
-//     $("#storjPriceMobile").html(storjPrice);
+  $("#factPrice").html(factPrice);
+  $("#factPriceMobile").html(factPrice);
+  $("#storjPrice").html(storjPrice);
+  $("#storjPriceMobile").html(storjPrice);
 
-//     $("#factNeeded").html(factNeeded);
-//     $("#factNeededMobile").html(factNeeded);
+  $("#factNeeded").html(factNeeded);
+  $("#factNeededMobile").html(factNeeded);
 
-//     $("#storjNeeded").html(storjNeeded);
-//     $("#storjNeededMobile").html(storjNeeded);
+  $("#storjNeeded").html(storjNeeded);
+  $("#storjNeededMobile").html(storjNeeded);
 
-//     $("#ethGasPrice").html(etheriumNeeded);
-// 	      $("#ethGasPrice2").html(etheriumNeeded);
-//     $("#ethGasPrice3").html(etheriumNeeded);
+  $("#ethGasPrice").html(etheriumNeeded);
+  $("#ethGasPrice2").html(etheriumNeeded);
+  $("#ethGasPrice3").html(etheriumNeeded);
 
-//     $("#hercNeeded").html(hercNeededFCT);
-//     $("#hercNeeded2").html(hercNeededSTORJ);
-//     $("#hercNeededMobile").html(hercNeededFCT);
-//     $("#hercNeededMobile2").html(hercNeededSTORJ);
-//     $("#ethGasPriceMobile").html(etheriumNeeded);
-// 	      $("#ethGasPriceMobile2").html(etheriumNeeded);
-//     $("#ethGasPriceMobile3").html(etheriumNeeded);
-
-//   });
+  $("#hercNeeded").html(hercNeededFCT);
+  $("#hercNeeded2").html(hercNeededSTORJ);
+  $("#hercNeededMobile").html(hercNeededFCT);
+  $("#hercNeededMobile2").html(hercNeededSTORJ);
+  $("#ethGasPriceMobile").html(etheriumNeeded);
+  $("#ethGasPriceMobile2").html(etheriumNeeded);
+  $("#ethGasPriceMobile3").html(etheriumNeeded);
+});
 // });
 </script>
 
@@ -208,6 +203,12 @@ table img {
 @media all and (max-width: 479px) {
   table td {
     font-size: 12px;
+  }
+
+  #table-section{
+    padding: 20px;
+    padding-top: 0px;
+    padding-bottom: 20px;
   }
 }
 </style>
