e515025375e83c4a1c3e481fe110d0a34942f458
daxdax89 <daxdax89@msn.com>
2018-09-07 21:31:56 +0200

    More Modals

--- src/components/CareersSection.vue
+++ src/components/CareersSection.vue
@@ -18,7 +18,6 @@
                         <h4><span>Remote</span><span>Engineering</span><span>Full-time</span></h4>
                     </div>
                     <div class="column right">
-                        <!-- <a href="mailto:daxdax89@msn.com">Apply</a> -->
                         <a @click="isCardModalActive = true">Apply</a>
                         <b-modal :active.sync="isCardModalActive" :width="640" scroll="keep">
                             <div class="card">
@@ -129,7 +128,95 @@
                         <h4><span>Remote</span><span>Engineering</span><span>Full-time</span></h4>
                     </div>
                     <div class="column right">
-                        <a href="mailto:daxdax89@msn.com">Apply</a>
+                        <a @click="isCardModalActive = true">Apply</a>
+                        <b-modal :active.sync="isCardModalActive" :width="640" scroll="keep">
+                            <div class="card">
+                                <div>
+                                    <div>
+                                        <h3 class="text-center">Infrastructure Engineer</h3>
+                                    </div>
+                                    <div class="modal-text">
+                                        <p><b>We seek an outstanding Infrastructure Engineer to help lay
+                                             the foundation for a more resilient, more secure, decentralized version of the web.
+                                        </b></p>
+                                        <h5>Engineering at HERC</h5>
+                                        <p>
+                                            Engineering at HERC is not just about writing world-class code. We create software systems
+                                             that tackle significant challenges. We aim to solve those challenges with new technology
+                                              breakthroughs and great user experience design.  We believe in an open-source approach
+                                               to creation. We strike a balance between pragmatism (put it on a ship ⛴), deeply
+                                                informed protocol design, and strict application of strong engineering principles.
+                                                 All of this happens in an environment defined by curiosity, passion, and a love for open source.
+                                        </p>
+                                        <h5>As a Infrastructure Engineer at HERC, you will...</h5>
+                                        <ul>
+                                            <li>
+                                                Develop tools and best practices for operating, debugging, and monitoring cutting edge Internet protocols such as IPFS and libp2p
+                                            </li>
+                                            <li>
+                                                Enable cross-functional product and project teams to serve their own deployment needs
+                                            </li>
+                                            <li>
+                                                Build and maintain CI/CD infrastructure for our open source projects
+                                            </li>
+                                            <li>
+                                                Work within the IPFS and Filecoin teams and communities to store Petabytes of data
+                                            </li>
+                                            <li>
+                                                Maintain the public HTTP-to-IPFS gateway, serving millions of requests per day
+                                            </li>
+                                            <li>
+                                                Manage our IP networks (we’re AS40680) and DNS
+                                            </li>
+                                            <li>
+                                                Come up with creative ways of interoperating with existing Internet infrastructure at all layers of the stack
+                                            </li>
+                                        </ul>
+                                        <h5>You may be a fit for this role if you have...</h5>
+                                        <ul>
+                                            <li>
+                                                Knowledge of core Internet protocols (BGP, IP, TCP, DNS, TLS, HTTP)
+                                            </li>
+                                            <li>
+                                                Experience managing fleets of Linux machines
+                                            </li>
+                                            <li>
+                                                Experience with container and cluster management tools (Nomad, Kubernetes, Docker)
+                                            </li>
+                                            <li>
+                                                Experience collaborating closely with software engineers, from design to development to production support
+                                            </li>
+                                            <li>
+                                                A hands-on, playful, proactive approach to your work
+                                            </li>
+                                        </ul>
+                                        <h5>Bonus Points...</h5>
+                                        <ul>
+                                            <li>
+                                                Experience contributing to or leading open source projects
+                                            </li>
+                                            <li>
+                                                Published about your work (code, presentations, papers, blog, etc)
+                                            </li>
+                                            <li>
+                                                Excellent written and verbal communication skills
+                                            </li>
+                                        </ul>
+                                        <p class="spacer"><b>What’s it like to work at HERC?</b></p>
+                                        <p class="spacer-bottom">
+                                            HERC's mission is to improve humanity’s most important technology, the Internet. 
+                                            We build protocols, systems, and tools to improve how it works. Today, we are focused on how we
+                                             store, locate, and move information. Our projects include IPFS, Filecoin, libp2p, and more.<br>
+                                            As a distributed team, we hire anywhere in the world, and at different levels of experience
+                                             (entry, senior, staff). We look for people with unique perspectives and diverse backgrounds.
+                                        </p>
+                                        <div class="text-center">
+                                            <a href="mailto:daxdax89@msn.com" class="modal-apply">Apply</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </b-modal>
                     </div>
                 </div>
                 <div class="columns">
@@ -138,7 +225,85 @@
                         <h4><span>Remote</span><span>Engineering</span><span>Full-time</span></h4>
                     </div>
                     <div class="column right">
-                        <a href="mailto:daxdax89@msn.com">Apply</a>
+                        <a @click="isCardModalActive = true">Apply</a>
+                        <b-modal :active.sync="isCardModalActive" :width="640" scroll="keep">
+                            <div class="card">
+                                <div>
+                                    <div>
+                                        <h3 class="text-center">Software Engineer, Blockchain</h3>
+                                    </div>
+                                    <div class="modal-text">
+                                        <p><b>We seek an outstanding Software Engineer to help us build the next generation of blockchain technology.
+                                        </b></p>
+                                        <h5>Engineering at HERC</h5>
+                                        <p>
+                                            Engineering at HERC is not just about writing world-class code. We create software systems
+                                             that tackle significant challenges. We aim to solve those challenges with new technology
+                                              breakthroughs and great user experience design.  We believe in an open-source approach
+                                               to creation. We strike a balance between pragmatism (put it on a ship ⛴), deeply
+                                                informed protocol design, and strict application of strong engineering principles.
+                                                 All of this happens in an environment defined by curiosity, passion, and a love for open source.
+                                        </p>
+                                        <h5>As a Software Engineer (Blockchain) at HERC, you might...</h5>
+                                        <ul>
+                                            <li>
+                                                Build the next generation of blockchain technology, developing projects like Filecoin
+                                            </li>
+                                            <li>
+                                                Participate in the design of performant and resilient applications
+                                            </li>
+                                            <li>
+                                                Integrate research results into real programs
+                                            </li>
+                                            <li>
+                                                Work alongside a cross-functional team including engineering, design, community, product, and business
+                                            </li>
+                                            <li>
+                                                Engage and educate the community, from users to contributors
+                                            </li>
+                                        </ul>
+                                        <h5>You may be a fit for this role if you have...</h5>
+                                        <ul>
+                                            <li>
+                                                Leadership experience building and maintaining large-scale distributed systems in production
+                                            </li>
+                                            <li>
+                                                Significant experience building applications using Go, Rust, or similar languages
+                                            </li>
+                                            <li>
+                                                Excellent understanding of blockchain technology
+                                            </li>
+                                            <li>
+                                                Deep understanding of network protocols
+                                            </li>
+                                            <li>
+                                                Experience working and contributing to Open Source projects
+                                            </li>
+                                            <li>
+                                                Excellent written and verbal communication skills
+                                            </li>
+                                            <li>
+                                                Experience leading and guiding Open Source projects (preferred)
+                                            </li>
+                                            <li>
+                                                Experience with 1 or more of the following: Smart Contracts, storage platforms, VMs (preferred)
+                                            </li>
+                                        </ul>
+                                        <p class="spacer"><b>What’s it like to work at HERC?</b></p>
+                                        <p class="spacer-bottom">
+                                            HERC's mission is to improve humanity’s most important technology, the Internet. 
+                                            We build protocols, systems, and tools to improve how it works. Today, we are focused on how we
+                                             store, locate, and move information. Our projects include IPFS, Filecoin, libp2p, and more.<br>
+                                            As a distributed team, we hire anywhere in the world, and at different levels of experience
+                                             (entry, senior, staff). We look for people with unique perspectives and diverse backgrounds.
+                                        </p>
+                                        <div class="text-center">
+                                            <a href="mailto:daxdax89@msn.com" class="modal-apply">Apply</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </b-modal>
                     </div>
                 </div>
                 <div class="columns">
@@ -477,8 +642,8 @@ span {
   margin-top: 5%;
 }
 
-.spacer-bottom{
-    margin-bottom: 5%;
+.spacer-bottom {
+  margin-bottom: 5%;
 }
 
 .modal-apply {
@@ -499,6 +664,33 @@ span {
 }
 /*** Responsive Styles Smartphone Only ***/
 @media all and (max-width: 767px) {
+  #job-listing {
+    padding: 50px;
+  }
+
+  .right {
+    text-align: center;
+  }
+
+  h2 {
+    text-align: center;
+  }
+
+  h3 {
+    text-align: center;
+  }
+
+  h4 {
+    text-align: center;
+  }
+
+  a {
+    font-size: 14px;
+  }
+
+  hr {
+    margin: auto;
+  }
 }
 /*** Responsive Styles Smartphone Portrait ***/
 @media all and (max-width: 479px) {
