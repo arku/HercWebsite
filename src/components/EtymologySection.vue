fb0b897e8d8aba30ad49e36fde42f70770ebfe9b
daxdax89 <daxdax89@msn.com>
2018-09-12 21:44:12 +0200

    Extra Code Removed

--- src/components/EtymologySection.vue
+++ src/components/EtymologySection.vue
@@ -37,16 +37,11 @@ export default {
   padding: 200px;
   padding-top: 50px;
   padding-bottom: 50px;
-  /* background-image: url("../assets/etymology/crane.png");
-  background-repeat: no-repeat;
-  background-size: inherit; */
-  /* background: linear-gradient(#091141, #1a245e); */
 }
 
 h1 {
   font-size: 45px;
   text-align: center;
-  /* color: #f3c736; */
   color: #091141;
   text-transform: uppercase;
   margin-bottom: 5%;
