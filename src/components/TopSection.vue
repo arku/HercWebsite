0f84398c1a9b65fb1aa45e4d1afc043b1f0e3c8b
daxdax89 <daxdax89@msn.com>
2018-09-21 20:11:37 +0200

    Temprarily Disabled Signups

--- src/components/TopSection.vue
+++ src/components/TopSection.vue
@@ -3918,51 +3918,35 @@ CzAAqYTVOPWO2yYAAAAASUVORK5CYII=" transform="matrix(0.98 0 0 0.98 1024.2817 776.
 
 	</svg>
             </div>
-			<div class="column">
+			<!-- <div class="column">
 				<img src="../assets/pillar-only-small.gif" id="animacija">
 				<div class="email-octopus-form-wrapper">
-    <h2 class="email-octopus-heading">Sign up for PRESALE!</h2>
-    <p class="email-octopus-success-message"></p>
-    <p class="email-octopus-error-message"></p>
-
-    <form method="post"
-      action="https://emailoctopus.com/lists/a1d42650-bcfd-11e8-a3c9-06b79b628af2/members/embedded/1.3s/add"
-      class="email-octopus-form"
-      data-sitekey="6LdYsmsUAAAAAPXVTt-ovRsPIJ_IVhvYBBhGvRV6"
-    >
-        
-        <div class="email-octopus-form-row">
-            <label for="field_0">Email address</label>
-            <input id="field_0" name="field_0" type="email" placeholder="">
-        </div>
-
-
-        
-
-        <div class="email-octopus-form-row-hp" aria-hidden="true">
-            <!-- Do not remove this field, otherwise you risk bot sign-ups -->
-            <input type="text" name="hpa1d42650-bcfd-11e8-a3c9-06b79b628af2"
-               tabindex="-1"
-               autocomplete="nope">
-        </div>
-
-        <div class="email-octopus-form-row-subscribe">
-            <input type="hidden"
-               name="successRedirectUrl"
-               value="">
-            <button type="submit">Subscribe</button>
-        </div>
-    </form>
-
-    
-</div>
-			</div>
-            <!-- <div class="column">
-				
-            <div id="subscribe-form">
-              <div class="ctct-inline-form" data-form-id="1b7fd750-5561-4cd8-a57d-7e6611366a52"></div>
-            </div>
-            </div> -->
+					<h2 class="email-octopus-heading">Sign up for PRESALE!</h2>
+					<p class="email-octopus-success-message"></p>
+					<p class="email-octopus-error-message"></p>
+					<form method="post"
+					action="https://emailoctopus.com/lists/a1d42650-bcfd-11e8-a3c9-06b79b628af2/members/embedded/1.3s/add"
+					class="email-octopus-form"
+					data-sitekey="6LdYsmsUAAAAAPXVTt-ovRsPIJ_IVhvYBBhGvRV6"
+					>
+						<div class="email-octopus-form-row">
+							<label for="field_0">Email address</label>
+							<input id="field_0" name="field_0" type="email" placeholder="">
+						</div>
+						<div class="email-octopus-form-row-hp" aria-hidden="true">
+							<input type="text" name="hpa1d42650-bcfd-11e8-a3c9-06b79b628af2"
+							tabindex="-1"
+							autocomplete="nope">
+						</div>
+						<div class="email-octopus-form-row-subscribe">
+							<input type="hidden"
+							name="successRedirectUrl"
+							value="">
+							<button type="submit">Subscribe</button>
+						</div>
+					</form>
+				</div>
+			</div> -->
           </div>
       </div>
     </div>
@@ -3987,9 +3971,9 @@ export default {
   background-color: #f3c736;
 }
 
-// svg {
-//   width: 60% !important;
-// }
+svg {
+  width: 60% !important;
+}
 
 h1 {
   font-size: 40px;
