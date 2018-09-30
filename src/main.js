529e574efd2e7cd5894be4dc6249512b58e1d03d
daxdax89 <daxdax89@msn.com>
2018-09-30 14:50:23 +0200

    TesingForms

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import Buefy from 'buefy'
 import vueSmoothScroll from 'vue-smooth-scroll'
 import BackToTop from 'vue-backtotop'
 
-Vue.use(Buefy,{
+Vue.use(Buefy, {
   defaultIconPack: "fa"
 });
 Vue.use(vueSmoothScroll)
@@ -13,6 +13,17 @@ Vue.use(BackToTop)
 Vue.config.productionTip = false
 
 new Vue({
+  mounted: function () {
+    this.$nextTick(function () {
+      const recaptchaScript = document.createElement('script');
+      recaptchaScript.src = 'https://emailoctopus.com/bundles/emailoctopuslist/js/1.3/recaptcha.js?1538041631';
+      document.querySelector('body').appendChild(recaptchaScript);
+
+      const mainScript = document.createElement('script');
+      mainScript.src = 'https://emailoctopus.com/bundles/emailoctopuslist/js/1.3/formEmbed.js?1538041631';
+      document.querySelector('body').appendChild(mainScript);
+    })
+  },
   router,
   render: h => h(App)
 }).$mount('#app')
