04c18bf4495464a6b59466c1c96ca440d3d1f776
daxdax89 <daxdax89@msn.com>
2018-10-01 18:09:08 +0200

    Removed Forms

--- src/main.js
+++ src/main.js
@@ -13,13 +13,6 @@ Vue.use(BackToTop)
 Vue.config.productionTip = false
 
 new Vue({
-  mounted: function () {
-    this.$nextTick(function () {
-      const script = document.createElement('script');
-      script.src = 'https://emailoctopus.com/bundles/emailoctopuslist/js/1.3/formEmbed.js?1538041631';
-      document.querySelector('body').appendChild(script);
-    })
-  },
   router,
   render: h => h(App)
 }).$mount('#app')
