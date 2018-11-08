b89719df6491a5d219c54d8e33037a3d4415d2b0
Danijel Demirovic <cucak92@gmail.com>
2018-11-08 14:16:47 +0100

    Update src/components/TeamSection.vue

--- src/components/TeamSection.vue
+++ src/components/TeamSection.vue
@@ -176,7 +176,7 @@
                         </a>
                     </div>
                     <div class="column">
-                        <img src="../assets/team/cule.jpeg">
+                        <img src="../assets/team/cule.jpg">
                         <h2 class="h2-desktop">Danijel Demirovic</h2>
                         <h3 class="h3-desktop">Junior Web Developer</h3>
                         <a href="https://www.linkedin.com/in/danijel-demirovic-671a79120/" target="_blank">
