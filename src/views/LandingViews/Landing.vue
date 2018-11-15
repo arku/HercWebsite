1572004e030a0e2a718f1548f854cc513f1a4c34
DaX <daxdax89@msn.com>
2018-11-15 15:27:26 +0100

    Cleaning

--- src/views/LandingViews/Landing.vue
+++ src/views/LandingViews/Landing.vue
@@ -2,7 +2,6 @@
     <div class="landing">
         <NavBar></NavBar>
         <LandingPageOne></LandingPageOne>
-        <!-- <SubscribeTest></SubscribeTest> -->
         <FooterSection></FooterSection>
         <back-to-top bottom="100px" right="10px">
           <button type="button" class="btn btn-info btn-to-top"><i class="fa fa-chevron-up"></i></button>
@@ -13,15 +12,13 @@
 import LandingPageOne from "@/components/LandingPages/LandingPageOne.vue";
 import FooterSection from "@/components/FooterSection.vue";
 import NavBar from "@/components/NavBar.vue";
-import SubscribeTest from "@/components/SubscribeTest.vue";
 
 export default {
   name: "Landing",
   components: {
     NavBar,
     LandingPageOne,
-    FooterSection,
-    SubscribeTest
+    FooterSection
   }
 };
 </script>
