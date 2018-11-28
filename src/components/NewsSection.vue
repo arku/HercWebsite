104d8206fdb50c5be981989b0890be8edab9784b
daxdax89 <daxdax89@msn.com>
2018-11-28 21:45:38 +0100

    Added new article

--- src/components/NewsSection.vue
+++ src/components/NewsSection.vue
@@ -1,49 +1,121 @@
 <template>
-    <div id="news-section">
-        <div id="content">
-            <h1><span>HERC</span> News</h1>
-            <div class="columns">
-                <div class="column">
-                    <blockquote class="embedly-card"><h4><a href="https://medium.com/@herc_token/herc-token-lists-on-la-token-deposits-now-open-eb539fb9b14c">Herc Token Lists on LA TOKEN - Deposits now open! - HERC Token - Medium</a></h4><p>LATOKEN opens deposits for Hercules (HERC)Deposit HERC NOW: https://bit.ly/2EShiwQ Please wait for further updates. Visit https://herc.one LATOKEN is a rapidly growing crypto exchange for new tokens with more than 80,000 registered users, and 180+ crypto pairs available.</p></blockquote>
-                </div>
-                <div class="column">
-                    <blockquote class="embedly-card"><h4><a href="https://medium.com/@herc_token/supporting-supply-chain-innovation-a-sound-digital-asset-strategy-f78424175cef">Supporting Supply Chain Innovation: A Sound Digital Asset Strategy?</a></h4><p>Fact: Current Supply Chain solutions are expensive. It is also a fact that companies with global supply chains could be standing on a cost base of which 90% is attributable to supply chain expenditure. The constant question for entrepreneurs, small companies, and large companies alike is: Can costs be saved by improving, streamlining, and optimizing the supply chain process?</p></blockquote>
-                </div>
-            </div>
-            <div class="columns">
-                <div class="column">
-                    <blockquote class="embedly-card"><h4><a href="https://medium.com/@herc_token/hercules-the-future-of-supply-chain-management-on-the-blockchain-37126381b26a">Hercules - The Future of Supply Chain Management on the Blockchain</a></h4><p>Supply Chains Today Just as the mythical stories about Hercules, the Roman god and hero, are full of adventures, so is the background story of Hercules ' blockchain-based supply chain management software solution.</p></blockquote>
-                </div>
-                <div class="column">
-                    <blockquote class="embedly-card"><h4><a href="https://medium.com/@herc_token/hercules-a-blockchain-based-supply-chain-optimization-solution-b41dc5dce3df">Hercules: a Blockchain-Based Supply Chain Optimization Solution</a></h4><p>Blockchain has tremendous potential for transforming supply chains and disrupting the way we produce, market, purchase and consume goods. Managing supply chains involves managing flows between the source of the raw materials all the way to the end customer.</p></blockquote>
-                </div>
-            </div>
-            <div class="columns">
-                <div class="column">
-                    <blockquote class="embedly-card"><h4><a href="https://medium.com/@herc_token/measure-track-verify-the-key-to-understanding-hercules-d80247756f57">Measure, Track, Verify - The Key to Understanding Hercules</a></h4><p>The industry standard for supply chain management does not currently offer a real-time view of all transactions to all the parties involved in a supply chain. This significant challenge is addressed by Hercules, which is built on the blockchain and enables immutable data to be shared among network participants and updated in real time.</p></blockquote>
-                </div>
-                <div class="column">
-                    <blockquote class="embedly-card"><h4><a href="https://medium.com/@herc_token/the-next-generation-of-digital-supply-chain-networks-1b6ce5e954f">The Next Generation of Digital Supply Chain Networks</a></h4><p>It is becoming increasingly apparent that supply chains across industries will be reimagined and revamped by using blockchain. This transformational technology offers a much safer and more transparent way to connect businesses and track and/or exchange any type of asset. Blockchains offer a solution to the growing problem of managing sophisticated networks of manufacturers and suppliers.</p></blockquote>
-                </div>
-            </div>
-            <div class="columns">
-                <div class="column">
-                    <blockquote class="embedly-card"><h4><a href="https://medium.com/@herc_token/how-blockchain-can-enhance-responsiveness-and-efficiency-in-global-value-chains-619fe61f9c26">How Blockchain Can Enhance Responsiveness and Efficiency in Global Value Chains</a></h4><p>Supply chain management involves planning, implementing and controlling supply chain operations, which typically include the movement and storage of raw materials, work-in-process inventory and finished goods from the point-of-origin to the point-of-consumption. The difficulty, however, comes with trying to find the best supply chain configuration that enables operations to be run efficiently and responsively.</p></blockquote>
-                </div>
-                <div class="column">
-                    <blockquote class="embedly-card"><h4><a href="https://medium.com/@herc_token/transforming-the-supply-chain-landscape-with-blockchain-a58d8e61b686">Transforming the Supply Chain Landscape With Blockchain</a></h4><p>Blockchain technology creates many possibilities within the supply chain space. From a historical view, the industrial era brought about the centralization of production. Mass manufacturing, for example, helped make centralized production more efficient. This centralization came in the form of large factories where raw commodities were turned into finished goods.</p></blockquote>
-                </div>
-            </div>
-            <div class="columns">
-                <div class="column">
-                    <blockquote class="embedly-card"><h4><a href="https://medium.com/@herc_token/integrating-blockchain-technology-into-supply-chains-17ea8eb6bb40">Integrating Blockchain Technology Into Supply Chains</a></h4><p>Blockchain technology has emerged as a disruptive technology that has the ability to transform supply chain ecosystems by offering an unprecedented level of transparency, which is hoped will result in greater supply chain management efficiency.</p></blockquote>
-                </div>
-                <div class="column">
-                    
-                </div>
-            </div>
+  <div id="news-section">
+    <div id="content">
+      <h1>
+        <span>HERC</span> News
+      </h1>
+      <div class="columns">
+        <div class="column">
+          <blockquote class="embedly-card">
+            <h4>
+              <a
+                href="https://www.apnews.com/c900c45285e44724bd67a4eb0729ec7e"
+              >Hercules's Highly-Anticipated Supply Chain Protocol Lists On Dobitrade</a>
+            </h4>
+            <p>GEORGETOWN, Cayman Islands--(BUSINESS WIRE)--Nov 28, 2018--HERC, the software access key token used to power Hercules Supply Chain Protocol, is now listed on DOBITRADE. HERC's listing on Hong Kong-based DOBITRADE facilitates and finally brings to reality Hercules SEZC's (the team behind the Hercules Protocol) goal of providing 100% visibility into all operations, processes and transactions through supply chain management on the blockchain.</p>
+          </blockquote>
         </div>
+        <div class="column">
+          <blockquote class="embedly-card">
+            <h4>
+              <a
+                href="https://medium.com/@herc_token/herc-token-lists-on-la-token-deposits-now-open-eb539fb9b14c"
+              >Herc Token Lists on LA TOKEN - Deposits now open! - HERC Token - Medium</a>
+            </h4>
+            <p>LATOKEN opens deposits for Hercules (HERC)Deposit HERC NOW: https://bit.ly/2EShiwQ Please wait for further updates. Visit https://herc.one LATOKEN is a rapidly growing crypto exchange for new tokens with more than 80,000 registered users, and 180+ crypto pairs available.</p>
+          </blockquote>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column">
+          <blockquote class="embedly-card">
+            <h4>
+              <a
+                href="https://medium.com/@herc_token/supporting-supply-chain-innovation-a-sound-digital-asset-strategy-f78424175cef"
+              >Supporting Supply Chain Innovation: A Sound Digital Asset Strategy?</a>
+            </h4>
+            <p>Fact: Current Supply Chain solutions are expensive. It is also a fact that companies with global supply chains could be standing on a cost base of which 90% is attributable to supply chain expenditure. The constant question for entrepreneurs, small companies, and large companies alike is: Can costs be saved by improving, streamlining, and optimizing the supply chain process?</p>
+          </blockquote>
+        </div>
+        <div class="column">
+          <blockquote class="embedly-card">
+            <h4>
+              <a
+                href="https://medium.com/@herc_token/hercules-the-future-of-supply-chain-management-on-the-blockchain-37126381b26a"
+              >Hercules - The Future of Supply Chain Management on the Blockchain</a>
+            </h4>
+            <p>Supply Chains Today Just as the mythical stories about Hercules, the Roman god and hero, are full of adventures, so is the background story of Hercules ' blockchain-based supply chain management software solution.</p>
+          </blockquote>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column">
+          <blockquote class="embedly-card">
+            <h4>
+              <a
+                href="https://medium.com/@herc_token/hercules-a-blockchain-based-supply-chain-optimization-solution-b41dc5dce3df"
+              >Hercules: a Blockchain-Based Supply Chain Optimization Solution</a>
+            </h4>
+            <p>Blockchain has tremendous potential for transforming supply chains and disrupting the way we produce, market, purchase and consume goods. Managing supply chains involves managing flows between the source of the raw materials all the way to the end customer.</p>
+          </blockquote>
+        </div>
+        <div class="column">
+          <blockquote class="embedly-card">
+            <h4>
+              <a
+                href="https://medium.com/@herc_token/measure-track-verify-the-key-to-understanding-hercules-d80247756f57"
+              >Measure, Track, Verify - The Key to Understanding Hercules</a>
+            </h4>
+            <p>The industry standard for supply chain management does not currently offer a real-time view of all transactions to all the parties involved in a supply chain. This significant challenge is addressed by Hercules, which is built on the blockchain and enables immutable data to be shared among network participants and updated in real time.</p>
+          </blockquote>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column">
+          <blockquote class="embedly-card">
+            <h4>
+              <a
+                href="https://medium.com/@herc_token/the-next-generation-of-digital-supply-chain-networks-1b6ce5e954f"
+              >The Next Generation of Digital Supply Chain Networks</a>
+            </h4>
+            <p>It is becoming increasingly apparent that supply chains across industries will be reimagined and revamped by using blockchain. This transformational technology offers a much safer and more transparent way to connect businesses and track and/or exchange any type of asset. Blockchains offer a solution to the growing problem of managing sophisticated networks of manufacturers and suppliers.</p>
+          </blockquote>
+        </div>
+        <div class="column">
+          <blockquote class="embedly-card">
+            <h4>
+              <a
+                href="https://medium.com/@herc_token/how-blockchain-can-enhance-responsiveness-and-efficiency-in-global-value-chains-619fe61f9c26"
+              >How Blockchain Can Enhance Responsiveness and Efficiency in Global Value Chains</a>
+            </h4>
+            <p>Supply chain management involves planning, implementing and controlling supply chain operations, which typically include the movement and storage of raw materials, work-in-process inventory and finished goods from the point-of-origin to the point-of-consumption. The difficulty, however, comes with trying to find the best supply chain configuration that enables operations to be run efficiently and responsively.</p>
+          </blockquote>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column">
+          <blockquote class="embedly-card">
+            <h4>
+              <a
+                href="https://medium.com/@herc_token/transforming-the-supply-chain-landscape-with-blockchain-a58d8e61b686"
+              >Transforming the Supply Chain Landscape With Blockchain</a>
+            </h4>
+            <p>Blockchain technology creates many possibilities within the supply chain space. From a historical view, the industrial era brought about the centralization of production. Mass manufacturing, for example, helped make centralized production more efficient. This centralization came in the form of large factories where raw commodities were turned into finished goods.</p>
+          </blockquote>
+        </div>
+        <div class="column">
+          <blockquote class="embedly-card">
+            <h4>
+              <a
+                href="https://medium.com/@herc_token/integrating-blockchain-technology-into-supply-chains-17ea8eb6bb40"
+              >Integrating Blockchain Technology Into Supply Chains</a>
+            </h4>
+            <p>Blockchain technology has emerged as a disruptive technology that has the ability to transform supply chain ecosystems by offering an unprecedented level of transparency, which is hoped will result in greater supply chain management efficiency.</p>
+          </blockquote>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
