539d29f5b4c43dbdcf4b5fbef3e715420a79246c
daxdax89 <daxdax89@msn.com>
2018-12-06 17:28:20 +0100

    Updated comment

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -159,15 +159,6 @@ export default {
     };
   }
 };
-
-// $(document).ready(function() {
-//   // Check for click events on the navbar burger icon
-//   $(".navbar-burger").click(function() {
-//     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-//     $(".navbar-burger").toggleClass("is-active");
-//     $(".navbar-menu").toggleClass("is-active");
-//   });
-// });
 </script>
 
 <style scoped lang="scss">
