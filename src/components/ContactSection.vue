3be2b7fc801857e7fb8570a4848ff9acd8f8fc65
daxdax89 <daxdax89@msn.com>
2018-12-10 23:22:09 +0100

    Email Octopus added

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -49,6 +49,40 @@
           </div>
         </div>
       </div>
+
+      <!-- Email Octopus -->
+      <div class="email-octopus-form-wrapper">
+        <h2 class="email-octopus-heading">Subscribe to HERC Website Widgets</h2>
+        <p class="email-octopus-success-message"></p>
+        <p class="email-octopus-error-message"></p>
+
+        <form
+          method="post"
+          action="https://emailoctopus.com/lists/a1d42650-bcfd-11e8-a3c9-06b79b628af2/members/embedded/1.3/add"
+          class="email-octopus-form"
+          data-sitekey="6LdYsmsUAAAAAPXVTt-ovRsPIJ_IVhvYBBhGvRV6"
+        >
+          <div class="email-octopus-form-row">
+            <label for="field_0">Email address</label>
+            <input id="field_0" name="field_0" type="email" placeholder>
+          </div>
+
+          <div class="email-octopus-form-row-hp" aria-hidden="true">
+            <!-- Do not remove this field, otherwise you risk bot sign-ups -->
+            <input
+              type="text"
+              name="hpa1d42650-bcfd-11e8-a3c9-06b79b628af2"
+              tabindex="-1"
+              autocomplete="nope"
+            >
+          </div>
+
+          <div class="email-octopus-form-row-subscribe">
+            <input type="hidden" name="successRedirectUrl" value>
+            <button type="submit">Subscribe</button>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
 </template>
